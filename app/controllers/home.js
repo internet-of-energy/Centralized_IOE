3e9206994ece0861e0d56a573ef7a97dcf57d69a
Akilan1999 <Akilanselva@hotmail.com>
2019-05-26 19:20:51 +0400

    UpDeveloping home page

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -3,14 +3,22 @@ var DB_config = require('../../config/database.js');
 //This shows the cheapest electrictiy avaliable in the market
 exports.home = function(req, res) {
 
-	console.log(req.session.user);
+	console.log(req.session.username);
+
+	if(req.session.username != 'undefined'){
 
 	DB_config.connection.query("SELECT users.name as name,Total_KwH,Cost_per_KwH FROM users,sellers" +
 	" where users.user_id = sellers.seller_id order by Cost_per_KwH limit 10;",
 	function (err, result, fields) {
+
     if (err) throw err;
 		res.render('index.ejs',{sellers:result});
   });
+	  
+  }
+  else{
+	 res.redirect('/login');
+  }
 }
 
 //This allows the customer to sell specific amount of electricity in the market
--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,11 +1,11 @@
 var DB_config = require('../../config/database.js');
 
+//renders the login page
 exports.login = function(req, res) {
-
 	res.render('login.ejs');
 }
 
-//Login validation 
+//Login validation : Check and if true redirects to the home page else shows the error
 exports.check = function(req, res) {
 	var Username = req.body.Username;
 	var Password = req.body.Password;
@@ -13,8 +13,9 @@ exports.check = function(req, res) {
 	if (Username && Password) {
 		DB_config.connection.query('SELECT * FROM users WHERE username = ? AND password = ?', [Username, Password], function(error, result, fields) {
 			if (result.length > 0) {
-				req.session.user_id = result.user_id;
-				req.session.username = result.username;
+				
+				req.session.user_id = result[0].user_id;
+				req.session.username = result[0].username;
 
 				res.redirect('/home');
 			} else {
